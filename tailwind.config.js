2e135e00fb475cc79cb24a91b2783cdbdab1abe9
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-11 12:27:14 -0300

    creating card rating

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ tailwind.config = {
                 'lightGrey':'#959EAC',
                 'darkBlue':'#252D37',
                 'varyDarkBlue':'#121417',
-            },       
+            },    
+             backgroundImage: {
+                'custom-radial': 'radial-gradient(circle, rgba(35, 42, 52, 1) 0%, rgba(24, 30, 39, 1) 100%)',
+            },   
         }
     }
 }
