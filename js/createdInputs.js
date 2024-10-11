e8e92ae3088ab3ec0c72f68e3ce9176a8a5bb3c0
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-11 13:39:30 -0300

    js to create input elements

---
+++ js/createdInputs.js
@@ -0,0 +1,21 @@
+const fild = document.querySelector('fieldset');
+
+for (let i = 0; i < 5; i++ ) {
+    const divInputElement = document.createElement('div');
+    divInputElement.classList.add("bg-darkBlue", "rounded-full", "relative", "p-3", "hover:text-black", "hover:bg-white");
+
+    const inputElement = document.createElement('input');
+    inputElement.classList.add("hidden");
+    inputElement.setAttribute('type', 'radio');
+    inputElement.setAttribute('value', i + 1);
+    inputElement.setAttribute('name', 'rating');
+    inputElement.setAttribute('id', `rating-${i + 1}`);
+
+    const labelElement = document.createElement('label');
+    labelElement.classList.add("relative", "flex", "items-center", "justify-center", "cursor-pointer");
+    labelElement.innerText = i + 1;
+
+    fild.appendChild(divInputElement);
+    divInputElement.appendChild(inputElement);
+    divInputElement.appendChild(labelElement);
+}
