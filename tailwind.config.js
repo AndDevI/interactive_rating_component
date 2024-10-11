3a74b3a6b55367772f41910d8c514edc6e9493d9
Andrew Raphael <andrewrgpires@gmail.com>
2024-10-11 08:35:06 -0300

    configuring html to use tailwind and adding color and font variables to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'overpass':['Overpass', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                'orange': '#FB7413',
+                'lightGrey':'#959EAC',
+                'darkBlue':'#252D37',
+                'varyDarkBlue':'#121417',
+            },       
+        }
+    }
+}
